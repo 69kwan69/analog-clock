0352379f2625bfe8a4b1d8e8a8191458a05a2939
69kwan69 <69hoangphi69@gmail.com>
2023-04-18 16:58:56 +0700

    change the hour hand to be more precise

--- analogClock.js
+++ analogClock.js
@@ -1,17 +1,17 @@
 function updateTime() {
     const nowTime = new Date();
 
-    const hour = nowTime.getHours();
-    const hourHand = document.querySelector('.hand.hour');
-    hourHand.style.rotate = `${hour * 30}deg`;
+    const secHand = document.querySelector('.hand.second');
+    const sec = nowTime.getSeconds();
+    secHand.style.rotate = `${sec * 6}deg`;
 
     const min = nowTime.getMinutes();
     const minHand = document.querySelector('.hand.minute');
     minHand.style.rotate = `${min * 6}deg`;
 
-    const secHand = document.querySelector('.hand.second');
-    const sec = nowTime.getSeconds();
-    secHand.style.rotate = `${sec * 6}deg`;
+    const hour = nowTime.getHours();
+    const hourHand = document.querySelector('.hand.hour');
+    hourHand.style.rotate = `${(hour + min / 60) * 30}deg`;
 }
 
 setInterval(updateTime, 1000);
